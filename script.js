5ab9e3b31623a92713a8ec666ad8b8f81b25ac7d
Tony Ganch <tonyganch@gmail.com>
2014-07-07 14:42:30 +0400

    Use CSScomb Core

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 // Require:
-var Comb = require('csscomb');
+var Comb = require('csscomb-core');
 var ruleJoiner = require('./rule-joiner/rule-joiner.js');
 
 // Configure:
-var comb = new Comb();
+var comb = new Comb([ruleJoiner], 'css');
 
 var css = '#a,  .b  {\n  width: 10px\n}\n#a,.b{\n  /*z*/foo:bar\n}\n/* hey ho */\n.b, #a {\n  bar: baz; \n}'
 
 var result = comb
-    .use(ruleJoiner)
     .configure({
-        'rule-joiner': true,
-        'always-semicolon': true,
-        'space-after-colon': ' ',
-        'sort-order': ['foo', 'bar', 'width']
+        'rule-joiner': true
     })
     .processString(css, 'css')
 
